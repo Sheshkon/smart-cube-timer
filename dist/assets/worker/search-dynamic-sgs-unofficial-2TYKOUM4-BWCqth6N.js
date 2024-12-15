cfe6fe206801856c3cc386ae105d463c49a8ea9b
sheshkon <sheskoalexei@gmail.com>
2024-12-15 17:53:28 +0300

    added stats

--- dist/assets/worker/search-dynamic-sgs-unofficial-2TYKOUM4-BWCqth6N.js
+++ dist/assets/worker/search-dynamic-sgs-unofficial-2TYKOUM4-BWCqth6N.js
@@ -1,4 +1,4 @@
-import{p as H}from"./chunk-Y3BVWVFU-BDrNbgoW.js";import{p as N,A as E,a as y}from"./inside-Y2UVJZNJ-Cu-MpFSH.js";import"../search-worker-entry-DNPX3OpU.js";var J=null;async function _B(){return J??(J=K())}async function K(){return H(await N.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
+import{p as H}from "./chunk-Y3BVWVFU-BDrNbgoW.js";import{p as N,A as E,a as y}from "./inside-Y2UVJZNJ-Cu-MpFSH.js";import"../search-worker-entry-DNPX3OpU.js";var J=null;async function _B(){return J??(J=K())}async function K(){return H(await N.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
 
 Alg T
 Alg B
