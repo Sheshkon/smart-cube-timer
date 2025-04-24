b4bf3794cc4de57b173e082326a5cdece24f24ab
sheshko <sheshkoalexei@gmail.com>
2025-04-25 00:44:38 +0300

    new vers

--- dist/assets/search-dynamic-sgs-unofficial-ZYGVRJQK-Briq33KX.js
+++ dist/assets/search-dynamic-sgs-unofficial-ZYGVRJQK-Briq33KX.js
@@ -1,4 +1,4 @@
-import{p as H}from"./chunk-BQNTAVTA-NXDU95ua.js";import{p as N,A as E,v as y}from"./index-UxbsTkP8.js";var J=null;async function pB(){return J??(J=K())}async function K(){return H(await N.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
+import{p as H}from"./chunk-BQNTAVTA-BS17FS0W.js";import{p as N,A as E,v as y}from"./index-D2ib8dGi.js";var J=null;async function pB(){return J??(J=K())}async function K(){return H(await N.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
 
 Alg T
 Alg B
