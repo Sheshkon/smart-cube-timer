1637cee54746e35442e84b4be7b73fe81cf16067
sheshko <sheshkoalexei@gmail.com>
2025-05-04 15:40:42 +0300

    added reconstruction logic

--- dist/assets/search-dynamic-sgs-unofficial-ZYGVRJQK.Bor0WlVl.js
+++ dist/assets/search-dynamic-sgs-unofficial-ZYGVRJQK.Bor0WlVl.js
@@ -1,4 +1,4 @@
-import{p as H}from "dist/assets/chunk-BQNTAVTA.B54hpPqa.js";import{A as N,p as E,v as y}from "dist/assets/index.MZ8s90wo.js";var J=null;async function pB(){return J??(J=K())}async function K(){return H(await E.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
+import{p as H}from"./chunk-BQNTAVTA.B54hpPqa.js";import{A as N,p as E,v as y}from"./index.MZ8s90wo.js";var J=null;async function pB(){return J??(J=K())}async function K(){return H(await E.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
 
 Alg T
 Alg B
--- dist/worker/search-dynamic-sgs-unofficial-ZYGVRJQK.DGZs_XEm.js
+++ dist/worker/search-dynamic-sgs-unofficial-ZYGVRJQK.DGZs_XEm.js
@@ -1,4 +1,4 @@
-import{p as H}from "dist/worker/chunk-BQNTAVTA.Bv7aCOdx.js";import{A as N,p as E,a as y}from "dist/worker/inside-KAVAZETT.BSm9a2pu.js";import"dist/worker/search-worker-entry.CROPli9L.js";var J=null;async function _B(){return J??(J=K())}async function K(){return H(await E.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
+import{p as H}from"./chunk-BQNTAVTA.Bv7aCOdx.js";import{A as N,p as E,a as y}from"./inside-KAVAZETT.BSm9a2pu.js";import"./search-worker-entry.CROPli9L.js";var J=null;async function _B(){return J??(J=K())}async function K(){return H(await E.fto.kpuzzle(),`SubgroupSizes 24 12 11 12 12 11 10 9 11 8 10 9 7 8 10 10 6 9 8 5 7 6 5 4 4 8 7 3 6 6 360
 
 Alg T
 Alg B
