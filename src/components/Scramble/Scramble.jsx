b2802c47f7881dc070ef3e5b09664ab0447b13d2
sheshko <sheshkoalexei@gmail.com>
2025-05-09 19:58:06 +0300

    fixed files

--- src/components/Scramble/Scramble.jsx
+++ src/components/Scramble/Scramble.jsx
@@ -1,8 +1,8 @@
-import { randomScrambleForEvent } from 'cubing/scramble';
 import React, { useEffect } from 'react';
+
+import { randomScrambleForEvent } from 'cubing/scramble';
 import { getMoveComponent } from 'src/components/Scramble/svgMapper.js';
 import { TimerState } from 'src/components/Timer/util.js';
-
 import { useCube } from 'src/hooks/useCube';
 import { useSettings } from 'src/hooks/useSettings';
 import { prepareMoves } from 'src/utils/util.ts';
