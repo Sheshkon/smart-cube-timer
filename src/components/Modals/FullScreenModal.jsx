ab4b20bbba2dc8cc7861b5a0b14f58c164d467b1
alexei <sheskoalexei@gmail.com>
2025-11-22 17:45:08 +0300

    fixed dialog mod

--- src/components/Modals/FullScreenModal.jsx
+++ src/components/Modals/FullScreenModal.jsx
@@ -4,21 +4,26 @@ const FullScreenModal = ({ children }) => {
   const modalRef = useRef(null);
 
   useEffect(() => {
+    document.body.style.overflow = 'hidden';
     if (modalRef.current) {
       modalRef.current.focus();
     }
+    return () => {
+      document.body.style.overflow = '';
+    };
+
   }, []);
 
   return (
     <dialog
       ref={modalRef}
       open
-      className="fixed inset-0 z-50 bg-base-100 w-screen max-w-none overflow-hidden outline-none"
+      className="fixed inset-0 z-40 bg-base-100 w-screen max-w-none overflow-hidden outline-none"
       aria-modal="true"
       aria-labelledby="modal-title"
     >
       <div className=" bg-base-100 w-full flex justify-between items-center shadow-sm">
-      <div className="w-full h-[calc(100vh)] overflow-y-auto">
+      <div className="w-full h-[calc(100dvh)] overflow-y-auto">
         <div className="w-full max-w-[1800px] mx-auto">
           {children}
         </div>
